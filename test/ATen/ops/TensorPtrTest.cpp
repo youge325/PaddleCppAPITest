b6d337b9d32f0f08b223487963a63b37fd097a21
youge325 <A372707325@126.com>
2026-06-26 18:09:31 +0800

    Add Tensor const_data_ptr alignment coverage

--- test/ATen/ops/TensorPtrTest.cpp
+++ test/ATen/ops/TensorPtrTest.cpp
@@ -12,6 +12,9 @@ TEST(TensorBodyTest, PtrTest) {
   const float* const_ptr = t.const_data_ptr<float>();
   EXPECT_NE(const_ptr, nullptr);
 
+  const float* const_type_ptr = t.const_data_ptr<const float>();
+  EXPECT_NE(const_type_ptr, nullptr);
+
   const void* void_const_ptr = t.const_data_ptr();
   EXPECT_NE(void_const_ptr, nullptr);
 
@@ -27,6 +30,7 @@ TEST(TensorBodyTest, PtrTest) {
   file.createFile();
   file << "PtrTest ";
   file << "const_ptr[0]: " + std::to_string(const_ptr[0]) + "\n";
+  file << "const_type_ptr[0]: " + std::to_string(const_type_ptr[0]) + "\n";
 
   mut_ptr[0] = 5.0f;
   file << "mut_ptr[0]: " + std::to_string(mut_ptr[0]) + "\n";
